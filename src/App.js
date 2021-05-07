812d832653f4e448a8138671591a3d38a72a544a
wajidmlk <wajidmlk123@gmail.com>
2021-05-07 16:50:46 +0500

    07May21

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className={styles.er}>asd</div>
+    </div>
+  );
+}
+
+export default App;
